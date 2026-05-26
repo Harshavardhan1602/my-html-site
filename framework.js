8ebd164754298b272ae7a4dcd030eb68c3407a4d
Harshavardhan1602 <58845065+Harshavardhan1602@users.noreply.github.com>
2026-05-26 16:08:12 +0530

    Update Genesys Framework loading and add toggle function
    Refactor Genesys Framework loader and add softphone toggle function.

--- framework.js
+++ framework.js
@@ -1,13 +1,18 @@
-// Genesys Embeddable Framework Loader
+// Load Genesys Framework
 (function () {
   const script = document.createElement("script");
   script.src = "https://apps.usw2.pure.cloud/crm/embeddableFramework.js";
   script.async = true;
-
-  script.onload = function () {
-    console.log("Genesys Embeddable Framework Loaded");
-  };
-
   document.head.appendChild(script);
 })();
-``
+
+// Toggle Softphone Widget
+function toggleSoftphone() {
+  const widget = document.getElementById("softphoneWidget");
+
+  if (widget.style.display === "flex") {
+    widget.style.display = "none";
+  } else {
+    widget.style.display = "flex";
+  }
+}
